fffa06710eae1ec782d1db3d37f98f93b4d4f865
Mario0221c <mcelam00@estudiantes.unileon.es>
2019-03-24 12:08:04 +0100

    Métodos de la Transparencia 1

--- src/es/unileon/prg/date/Date.java
+++ src/es/unileon/prg/date/Date.java
@@ -37,6 +37,135 @@ public class Date {
 	}
 
 
+//MÉTODOS TRANSPARENCIA 1:
+
+	/*
+
+	Recibe otra fecha que se le pasa como parámetro y compara su año con el de los atributos de la clase, es decir con el de mi fecha. Si son iguales devuelve true, si no, false.
+
+	*/
+
+	public boolean isSameYearIF(Date fecha2){
+	
+		boolean esMismoAnio;
+			
+		if(this.year == fecha2.year){
+			esMismoAnio = true;
+		}
+		else
+		{
+			esMismoAnio = false;
+		}
+		return esMismoAnio;
+	}
+
+	/*
+
+	Recibe otra fecha que se le pasa como parámetro y compara su mes con el de los atributos de la clase, es decir con el de mi fecha. Si son iguales devuelve true, si no, false.
+
+	*/
+
+	public boolean isSameMonthIF(Date fecha2){ 
+
+		boolean esMismoMes;
+
+		if(this.month == fecha2.month){
+			esMismoMes = true;
+		}
+		else
+		{
+			esMismoMes = false;
+		}
+		return esMismoMes;
+	}
+
+	/*
+
+	Recibe otra fecha que se le pasa como parámetro y compara su día con el de los atributos de la clase, es decir con el de mi fecha. Si son iguales devuelve true, si no, false.
+
+	*/
+
+	public boolean isSameDayIF(Date fecha2){
+
+		boolean esMismoDia;
+
+		if(this.day == fecha2.year){
+			esMismoDia = true;
+		}
+		else
+		{
+			esMismoDia = false;
+		}
+		return esMismoDia;
+	}
+
+	/*
+
+	Recibe otra fecha que se le pasa como parámetro y la compara (entera) con mi fecha. Si son iguales devuelve true, si no, false.
+
+	*/
+
+	public boolean isSameIF(Date fecha2){
+
+		boolean esMismaFecha;
+		//miro a ver si metendo la fecha 2 en mi metodo que comprueba si es o no igual, sale true 
+		if(isSameDayIF(fecha2) == true && isSameMonthIF(fecha2) == true && this.year==fecha2.year){
+			esMismaFecha = true;
+		}
+		else
+		{
+			esMismaFecha = false;
+		}
+		return esMismaFecha;
+	}
+
+	/*
+
+	Recibe otra fecha que se le pasa como parámetro y compara su año con el de los atributos de la clase, es decir con el de mi fecha. Sin usar if, directamente devolverá un true si se cumple lo que está en el return y si no, un false, porque en la firma del método ya hemos dicho que devolverá un boolean.
+
+	*/
+
+	public boolean isSameYear(Date fecha2){
+	
+		return this.year == fecha2.year; 
+
+	}
+
+	/*
+
+	Recibe otra fecha que se le pasa como parámetro y compara su mes con el de los atributos de la clase, es decir con el de mi fecha. Sin usar if, directamente devolverá un true si se cumple lo que está en el return y si no, un false, porque en la firma del método ya hemos dicho que devolverá un boolean.
+
+	*/
+
+	public boolean isSameMonth(Date fecha2){
+
+		return this.month == fecha2.month;
+
+	}
+
+	/*
+
+	Recibe otra fecha que se le pasa como parámetro y compara su día con el de los atributos de la clase, es decir con el de mi fecha. Sin usar if, directamente devolverá un true si se cumple lo que está en el return y si no, un false, porque en la firma del método ya hemos dicho que devolverá un boolean.
+
+	*/
+
+	public boolean isSameDay(Date fecha2){
+
+		return this.day == fecha2.day;
+
+	}
+	
+	/*
+
+	Recibe otra fecha que se le pasa como parámetro y compara su fecha completa con mi fecha. Sin usar if, directamente devolverá un true si se cumple lo que está en el return y si no, un false, porque en la firma del método ya hemos dicho que devolverá un boolean.
+	*/
+
+	public boolean isSame(Date fecha2){
+
+		return this.isSameYear(fecha2) && this.isSameMonth(fecha2) && this.isSameDay(fecha2); 
+		//porque llamo a los metodos con la fecha de hoy, ese this es como si pusiera today.metodo
+
+	}
 
 
 
