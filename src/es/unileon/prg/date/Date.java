af09a6d0b4cac7e52f005c907911e594c141dbaf
Mario0221c <mcelam00@estudiantes.unileon.es>
2019-03-24 11:10:21 +0100

    Acabado el constructor de la clase

--- src/es/unileon/prg/date/Date.java
+++ src/es/unileon/prg/date/Date.java
@@ -2,86 +2,80 @@ package es.unileon.prg.date;
 
 public class Date {
 
-	private int _day;
-	private int _month;
-	private int _year;
+	private int day;
+	private int month;
+	private int year;
+	
+	/*
+
+	Constructor de la clase:
+		Asigna los parametros que recibe a los atributos de la clase Date, pero no se admiten fechas tales como 99/15/xxxx, entonces hay que plantear unas condiciones que no permitan que esas fechas funcionen y se lance una extepción.
+
+	*/
 
 	public Date(int day, int month, int year) throws DateException{
-		this._year = year;
+		this.year = year;
 		if (month < 1 || month > 12) {
 			throw new DateException("Mes " + month + " no valido" +
 					" Valores posibles entre 1 y 12.");
 		} else {
-			this._month = month;
+			this.month = month;
 		}
 		//TODO falta comprobar el dia
-/*
-		if((day > 0) && (day < getDaysOfMonth())){
-		this._day = day;
-		}
-		else
-		{
-			throw new DateException("Dia " + month + " no valido" +
-					" para el mes "+getMonthDay+" Valores posibles entre 1 y "+getDaysOfMonth());
-		}
-*/
-}
-
-/*	
-	//public String getMonthDay()
-	{
-
-
-		//switch con el nombre de los meses
-
-
-
-	}
-
-
-
-
-
-
-
-
-
-	//public int getDaysOfMonth(int month)
-	{
-		int numero = 0;
-		switch(numero)
-		{
-			case 1:
-			case 2:
-			case 3:
-			case 4:
-			case 5:
-			case 6:
-			case 7:
-			case 8:
-			case 9:
-			case 10:
-			case 11:
-			case 12:
-			
 		
-
-
-		}	
-
-
-
-
-
-
-
-
+		if (day < 1 || day > 31) {
+			throw new DateException("Día " + day + " no valido" +
+					" Valores posibles entre 1 y 31.");
+		} else {
+			this.day = day;
+		}
 	}
-*/
+
 	@Override
 	public String toString() {
-		return this._day + "/" + this._month + "/" + this._year;
+		return this.day + "/" + this.month + "/" + this.year;
 	}
 
-}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
