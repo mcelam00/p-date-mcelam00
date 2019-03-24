e1261bc2761ad9f6858262c0ed01537b54d8dc7e
Mario0221c <mcelam00@estudiantes.unileon.es>
2019-03-24 22:17:25 +0100

    Documentado

--- src/es/unileon/prg/date/Date.java
+++ src/es/unileon/prg/date/Date.java
@@ -14,12 +14,20 @@ public class Date {
 	private int month;
 	private int year;
 	
-	/*
+	/**
+	 * Constructor de la clase. Crea una fecha con la informacion recibida 
+	 * Asigna los parametros que recibe a los atributos de la clase Date.
+	 * No se admiten fechas tales como 99/15/xxxx, entonces hay que plantear unas condiciones que no permitan que esas fechas funcionen y se lance una extepción.
+	 * @param day
+	 *            Día de mi fecha
+	 * @param month
+	 *            Mes de mi fecha
+	 * @param year         
+	 *            Año de mi fecha
+	 *          
+	 */
+	
 
-	Constructor de la clase:
-		Asigna los parametros que recibe a los atributos de la clase Date, pero no se admiten fechas tales como 99/15/xxxx, entonces hay que plantear unas condiciones que no permitan que esas fechas funcionen y se lance una extepción.
-
-	*/
 
 	public Date(int day, int month, int year) throws DateException{
 		this.year = year;
@@ -49,12 +57,13 @@ public class Date {
 
 //MÉTODOS TRANSPARENCIA 1:
 
-	/*
-
-	Recibe otra fecha que se le pasa como parámetro y compara su año con el de los atributos de la clase, es decir con el de mi fecha. Si son iguales devuelve true, si no, false.
-
-	*/
-
+	/**
+	 * Método que recibe otra fecha que se le pasa como parámetro y compara su año con el de los atributos de la clase, es decir con el de mi fecha. Si son iguales devuelve true, si no, false.
+	 * 
+	 * @param fecha2
+	 *            La otra fecha que queremos comparar
+	 */
+	
 	public boolean isSameYearIF(Date fecha2){
 	
 		boolean esMismoAnio;
@@ -69,12 +78,13 @@ public class Date {
 		return esMismoAnio;
 	}
 
-	/*
-
-	Recibe otra fecha que se le pasa como parámetro y compara su mes con el de los atributos de la clase, es decir con el de mi fecha. Si son iguales devuelve true, si no, false.
-
-	*/
-
+	/**
+	 * Método que recibe otra fecha que se le pasa como parámetro y compara su mes con el de los atributos de la clase, es decir con el de mi fecha. Si son iguales devuelve true, si no, false.
+	 * 
+	 * @param fecha2
+	 *            La otra fecha que queremos comparar
+	 */
+	
 	public boolean isSameMonthIF(Date fecha2){ 
 
 		boolean esMismoMes;
@@ -89,11 +99,12 @@ public class Date {
 		return esMismoMes;
 	}
 
-	/*
-
-	Recibe otra fecha que se le pasa como parámetro y compara su día con el de los atributos de la clase, es decir con el de mi fecha. Si son iguales devuelve true, si no, false.
-
-	*/
+	/**
+	 * Método que recibe otra fecha que se le pasa como parámetro y compara su día con el de los atributos de la clase, es decir con el de mi fecha. Si son iguales devuelve true, si no, false.
+	 * 
+	 * @param fecha2
+	 *            La otra fecha que queremos comparar
+	 */
 
 	public boolean isSameDayIF(Date fecha2){
 
@@ -109,12 +120,13 @@ public class Date {
 		return esMismoDia;
 	}
 
-	/*
-
-	Recibe otra fecha que se le pasa como parámetro y la compara (entera) con mi fecha. Si son iguales devuelve true, si no, false.
-
-	*/
-
+	/**
+	 * Método que recibe otra fecha que se le pasa como parámetro y la compara (entera) con mi fecha. Si son iguales devuelve true, si no, false.
+	 * 
+	 * @param fecha2
+	 *            La otra fecha que queremos comparar
+	 */
+	
 	public boolean isSameIF(Date fecha2){
 
 		boolean esMismaFecha;
@@ -131,35 +143,41 @@ public class Date {
 		return esMismaFecha;
 	}
 
-	/*
-
-	Recibe otra fecha que se le pasa como parámetro y compara su año con el de los atributos de la clase, es decir con el de mi fecha. Sin usar if, directamente devolverá un true si se cumple lo que está en el return y si no, un false, porque en la firma del método ya hemos dicho que devolverá un boolean.
-
-	*/
-
+	/**
+	 * Método que recibe otra fecha que se le pasa como parámetro y compara su año con el de los atributos de la clase, es decir con el de mi fecha. 
+	 * Sin usar if, directamente devolverá un true si se cumple lo que está en el return y si no, un false, porque en la firma del método ya hemos dicho que devolverá un boolean.
+	 * 
+	 * @param fecha2
+	 *            La otra fecha que queremos comparar
+	 */
+	
 	public boolean isSameYear(Date fecha2){
 	
 		return this.year == fecha2.year; 
 
 	}
 
-	/*
-
-	Recibe otra fecha que se le pasa como parámetro y compara su mes con el de los atributos de la clase, es decir con el de mi fecha. Sin usar if, directamente devolverá un true si se cumple lo que está en el return y si no, un false, porque en la firma del método ya hemos dicho que devolverá un boolean.
-
-	*/
-
+	/**
+	 * Método que recibe otra fecha que se le pasa como parámetro y compara su mes con el de los atributos de la clase, es decir con el de mi fecha. 
+	 * Sin usar if, directamente devolverá un true si se cumple lo que está en el return y si no, un false, porque en la firma del método ya hemos dicho que devolverá un boolean.
+	 * 
+	 * @param fecha2
+	 *            La otra fecha que queremos comparar
+	 */
+	
 	public boolean isSameMonth(Date fecha2){
 
 		return this.month == fecha2.month;
 
 	}
 
-	/*
-
-	Recibe otra fecha que se le pasa como parámetro y compara su día con el de los atributos de la clase, es decir con el de mi fecha. Sin usar if, directamente devolverá un true si se cumple lo que está en el return y si no, un false, porque en la firma del método ya hemos dicho que devolverá un boolean.
-
-	*/
+	/**
+	 * Método que recibe otra fecha que se le pasa como parámetro y compara su día con el de los atributos de la clase, es decir con el de mi fecha. 
+	 * Sin usar if, directamente devolverá un true si se cumple lo que está en el return y si no, un false, porque en la firma del método ya hemos dicho que devolverá un boolean.
+	 * 
+	 * @param fecha2
+	 *            La otra fecha que queremos comparar
+	 */
 
 	public boolean isSameDay(Date fecha2){
 
@@ -167,11 +185,14 @@ public class Date {
 
 	}
 	
-	/*
-
-	Recibe otra fecha que se le pasa como parámetro y compara su fecha completa con mi fecha. Sin usar if, directamente devolverá un true si se cumple lo que está en el return y si no, un false, porque en la firma del método ya hemos dicho que devolverá un boolean.
-	*/
-
+	/**
+	 * Método que recibe otra fecha que se le pasa como parámetro y compara su fecha completa con mi fecha.
+	 * Sin usar if, directamente devolverá un true si se cumple lo que está en el return y si no, un false, porque en la firma del método ya hemos dicho que devolverá un boolean.
+	 * 
+	 * @param fecha2
+	 *            La otra fecha que queremos comparar
+	 */
+	
 	public boolean isSame(Date fecha2){
 
 		return this.isSameYear(fecha2) && this.isSameMonth(fecha2) && this.isSameDay(fecha2); 
@@ -183,13 +204,14 @@ public class Date {
 
 //MÉTODOS DE LA TRANSPARENCIA 2:
 
-	/*
-
-	El método devuelve el nombre del mes de mi fecha.
-	Mediante un switch mira el numero de mes que tiene mi fecha (this.month) y según el caso que sea me devuelve el nombre del mes almacenado en un string.
-
-	*/
-
+	/**
+	 * Método que devuelve el nombre del mes de mi fecha.
+	 * Mediante un switch mira el numero de mes que tiene mi fecha (this.month) y según el caso que sea me devuelve el nombre del mes almacenado en un string.
+	 * 
+	 * No se le pasa nada como parámetros puesto que solo usa los atributos de la clase.
+	 *          
+	 */
+	
 	public String nombreMes(){
 
 		String mes;
@@ -237,15 +259,18 @@ public class Date {
 		return mes;
 	}
 
-	/*
-
-		Método Auxiliar para usarlo en otros que me da el ultimo día del mes introducido:
-
-	Le paso como parámetro un mes genérico porque si no le pongo nada solo podría obtener el ultimo dia del mes de mi fecha, pero siendo así ese "int mes" generico tomará un valor u otro según que fecha lo llame. 
-
-	"Treinta días trae noviembre, con abril, junio y septiembre. Los demás treinta y uno, menos febrero mocho que trae veintiocho".
-
-	*/
+	/**
+	 * Método Auxiliar para usarlo en otros que me da el ultimo día del mes introducido:
+	 * Mediante un switch mira el numero de mes que tiene mi fecha (this.month) y según el caso que sea me devuelve el nombre del mes almacenado en un string.
+	 * 
+	 * 	  
+	 * @param mes
+	 *           mes genérico
+	 * Le paso como parámetro un mes genérico porque si no le pongo nada solo podría obtener el ultimo dia del mes de mi fecha.
+	 * siendo así ese "int mes" generico tomará un valor u otro según que fecha lo llame.         
+	 */
+	
+	//"Treinta días trae noviembre, con abril, junio y septiembre. Los demás treinta y uno, menos febrero mocho que trae veintiocho".
 
 	public int ultimoDiaMes(int mes){
 
@@ -272,13 +297,15 @@ public class Date {
 		return ultimoDia;
 	}
 
-	/*
-
-	El método mira si el dia del mes de mi fecha es correcto. (por ejemplo, que el 30 de febrero no valga) Como el del constructor pero especificamente para cada mes.
-Lo creo con if else, si no, bastaría con poner la condición "this.day <= this.ultimoDiaMes(this.month) && this.day >= 1" detrás del return como hago en los metodos de la transparencia 1 sin if.
-
-	*/
-
+	/**
+	 * Método que valida si el dia del mes de mi fecha es correcto  (por ejemplo, que el 30 de febrero no valga).
+	 * Lo creo con if else.
+	 * Si no, bastaría con poner la condición "this.day <= this.ultimoDiaMes(this.month) && this.day >= 1" detrás del return como hago en los metodos de la transparencia 1 sin if.
+	 * 
+	 * No se le pasa nada como parámetros puesto que solo usa los atributos de la clase.	  
+	 * 
+	 */
+	
 	public boolean esCorrectoDiaMes(){
 	
 		boolean esCorrecto; 
@@ -295,18 +322,18 @@ Lo creo con if else, si no, bastaría con poner la condición "this.day <= this.
 
 	}
 
-	/*
-
-	El método mira el mes en el que estamos y devuelve la estación correspondiente.
-	No se tienen en cuenta las particiones a mitad de mes, es decir, si la primavera comienza el 21 de Marzo, se toma todo marzo como invierno. 
+	/**
+	 * Método que devuelve la estación correspondiente al mes de la fecha.
+	 * No se tienen en cuenta las particiones a mitad de mes, es decir, si la primavera comienza el 21 de Marzo, se toma todo marzo como invierno. 
+	 * 
+	 * 	  
+	 * No se le pasa nada como parámetros puesto que solo usa los atributos de la clase.	  
+	 */
 	
-
-	Primavera: Comienza alrededor del 21 de marzo en el hemisferio norte.
-	Verano: Comienza alrededor del 21 de junio en el hemisferio norte.
-	Otoño: Comienza alrededor del 23 de septiembre en el hemisferio norte.
-	Invierno: Comienza alrededor del 21 de diciembre en el hemisferio norte.
-
-	*/
+	//Primavera: Comienza alrededor del 21 de marzo en el hemisferio norte.
+	//Verano: Comienza alrededor del 21 de junio en el hemisferio norte.
+	//Otoño: Comienza alrededor del 23 de septiembre en el hemisferio norte.
+	//Invierno: Comienza alrededor del 21 de diciembre en el hemisferio norte.
 
 	public String estacion(){
 
@@ -346,11 +373,13 @@ Lo creo con if else, si no, bastaría con poner la condición "this.day <= this.
 
 //MÉTODOS DE LA TRANSPARENCIA 3:
 
-	/*
-	El método me devuelve el nombre de los meses restantes desde el intoducido (no se incluye) hasta el final del año.
+	/**
+	 * Método que devuelve el nombre de los meses restantes desde el intoducido (no se incluye) hasta el final del año.
+	 * 
+	 * 	  
+	 * No se le pasa nada como parámetros puesto que solo usa los atributos de la clase.	
+	 */
 	
-	*/
-
 	public String mesesRestantes(){
 
 		Stringbuffer meses=new Stringbuffer();
@@ -404,12 +433,13 @@ Lo creo con if else, si no, bastaría con poner la condición "this.day <= this.
 			return meses;
 		}
 
-	/*
+	/**
+	 * Método que imprime la fecha en formato de cadena.
+	 * 
+	 * 	  
+	 * No se le pasa nada como parámetros puesto que solo usa los atributos de la clase.	
+	 */
 	
-	El método imprime la fecha en formato de cadena.
-
-	*/
-
 	public void fechaEscrita(){
 
 		Stringbuffer fecha=new Stringbuffer();
@@ -421,12 +451,14 @@ Lo creo con if else, si no, bastaría con poner la condición "this.day <= this.
 		System.out.println(fecha.toString());
 	}
 
-	/*
-	
-	El método genera todas las fechas desde la fecha establecida hasta la ultima fecha del mes, para ello, se vale del método auxiliar que hemos creado y que nos dice el ultimo dia del mes, y del método que convierte la fecha en cadena de texto y la muestra por pantalla.
-
-	*/
-	
+	/**
+	 * Método que genera todas las fechas desde la fecha establecida hasta la ultima fecha del mes.
+	 * Para ello, se vale del método auxiliar que hemos creado y que nos dice el ultimo dia del mes, y del método que convierte la fecha en cadena de texto y la muestra por pantalla.
+	 * 
+	 * 	  
+	 * No se le pasa nada como parámetros puesto que solo usa los atributos de la clase.	
+	 */
+		
 	public void ultimaFechaMes(){
 
 		int ultimoDia;
@@ -446,12 +478,14 @@ Lo creo con if else, si no, bastaría con poner la condición "this.day <= this.
 
 	}
 
-	/*
-
-	Método que retorna los meses con el mismo numero de dias que el mes de mi fecha.
-	Recorre todos los meses a ver cuales tienen los mismos dias que el de mi fecha, y los imprime.
-	*/
-
+	/**
+	 * Método que imprime los meses con el mismo numero de dias que el mes de mi fecha.
+	 * Recorre todos los meses a ver cuales tienen los mismos dias que el de mi fecha, y los imprime.
+	 *
+	 * 	  
+	 * No se le pasa nada como parámetros puesto que solo usa los atributos de la clase.	
+	 */
+	
 	public void mesesIguales(){
 	
 	//Con el for recorro todos los meses.
@@ -511,7 +545,14 @@ Lo creo con if else, si no, bastaría con poner la condición "this.day <= this.
 	}	
 	}
 
-
+	/**
+	 * Método que imprime el número de días que hay desde el 1 de Enero hasta mi fecha.
+	 * Recorre todos los meses a ver cuales tienen los mismos dias que el de mi fecha, y los imprime.
+	 *
+	 * 	  
+	 * No se le pasa nada como parámetros puesto que solo usa los atributos de la clase.	
+	 */
+	
 	public void numeroDiasDesdeInicioAño() {
 		int numeroDias=0;
 
@@ -534,9 +575,11 @@ Lo creo con if else, si no, bastaría con poner la condición "this.day <= this.
 //MÉTODOS TRANSPARENCIA 4:
 
 	/**
-
-
-	*/
+	 * Método que imprime el número de intentos necesarios para generar una fecha aleatoria que coincida con mi fecha.
+	 * Se utiliza el while.
+	 * 	  
+	 * No se le pasa nada como parámetros puesto que solo usa los atributos de la clase.	
+	 */
 
 	public void numeroIntentos(){
 		Random rnd = new Random();
@@ -555,6 +598,12 @@ Lo creo con if else, si no, bastaría con poner la condición "this.day <= this.
 
 	}
 
+	/**
+	 * Método que imprime el número de intentos necesarios para generar una fecha aleatoria que coincida con mi fecha.
+	 * Se utiliza el do-while.
+	 * 	  
+	 * No se le pasa nada como parámetros puesto que solo usa los atributos de la clase.	
+	 */
 
 	public void numeroIntentosDW() {
 
@@ -574,7 +623,14 @@ Lo creo con if else, si no, bastaría con poner la condición "this.day <= this.
 		System.out.println("El numero de intentos necesarios serían: "+intentos);
 	}
 
-// metodo auxiliar para el ultimo que me diga ese resto de la division modulo 7 a qué día de la semana se corresponde.
+	/**
+	 * Método auxiliar para el ultimo método que me dice a qué día de la semana se corresponde ese resto de la division modulo 7.
+	 * 
+	 * 
+	 * 	  
+	 * @param i
+	 *           Un entero entre 0 y 6 (el resto de la division módulo 7)
+	 */ 
 
 	public String NombreDelDia(int i) {
 
@@ -600,7 +656,7 @@ Lo creo con if else, si no, bastaría con poner la condición "this.day <= this.
 			break;
 		case 0:
 
-//Porque si divido y me da resto cero quiere decir que han pasado justo esas semanas pero no he empezado la siguiente semana, luego estoy en domingo.
+		//Porque si divido y me da resto cero quiere decir que han pasado justo esas semanas pero no he empezado la siguiente semana, luego estoy en domingo.
 
 			dia = "Domingo";
 			break;
@@ -609,8 +665,13 @@ Lo creo con if else, si no, bastaría con poner la condición "this.day <= this.
 	return dia;
 	}
 
-//lo que hace es decirme el numero de dias que hay desde el uno de enero hasta fecha actual
-
+	/**
+	 * Método que imprime el número de dias que hay desde el uno de enero hasta fecha actual.
+	 * 
+	 * 	  
+	 * No se le pasa nada como parámetros puesto que solo usa los atributos de la clase.	
+	 */
+	
 	public void DiaSemanaHoy(int dia1Enero) {
 		int numeroDias=0;
 		for(int i=1; i<this.month; i++) {
@@ -619,11 +680,11 @@ Lo creo con if else, si no, bastaría con poner la condición "this.day <= this.
 		numeroDias = numeroDias + this.day;
 		numeroDias = numeroDias + dia1Enero -1; 
 
-//hay que poner esto porque sino estas prefijando que el año empieza en lunes 1 y puede que el año empiece en miércoles 1. Los días entre la fecha y el día 1 se ven inalterados, pero si hacemos la división modulo 7 sale en los dos casos lo mismo y no es posible. Si pintamos en un papel los dos casos y hacemos el calendario veremos que la diferencia del día final entre empezar un lunes 1 y un miércoles 1 es de 2 días. Según nuestro método anterior, el lunes es la posición 1 de la semana y el miércoles es la posición 3 pero queremos que al número de días totales le sume 2 no 3, luego de ahí el -1.
+		//hay que poner esto porque sino estas prefijando que el año empieza en lunes 1 y puede que el año empiece en miércoles 1. Los días entre la fecha y el día 1 se ven inalterados, pero si hacemos la división modulo 7 sale en los dos casos lo mismo y no es posible. Si pintamos en un papel los dos casos y hacemos el calendario veremos que la diferencia del día final entre empezar un lunes 1 y un miércoles 1 es de 2 días. Según nuestro método anterior, el lunes es la posición 1 de la semana y el miércoles es la posición 3 pero queremos que al número de días totales le sume 2 no 3, luego de ahí el -1.
 
 		int diaSemana = numeroDias%7;
 
-//esos dias que hay entre medias del primer dia de la primera semana del año y mi fecha los divido entre siete para saber las semanas que han sido (los ciclos completos) y el resto que es el dia de la semana en el que estoy realmente (la posicion en el ciclo siguiente) que será el que meteré en el otro método y me dirá a qué día de la semana se corresponde.
+		//esos dias que hay entre medias del primer dia de la primera semana del año y mi fecha los divido entre siete para saber las semanas que han sido (los ciclos completos) y el resto que es el dia de la semana en el que estoy realmente (la posicion en el ciclo siguiente) que será el que meteré en el otro método y me dirá a qué día de la semana se corresponde.
 
 		System.out.println("hoy es: "+NombreDia(diaSemana));
 		
